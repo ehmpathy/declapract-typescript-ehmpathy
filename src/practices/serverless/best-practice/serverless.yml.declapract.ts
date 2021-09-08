81d9c4a196b77b76d8d44e7164b3cd91283275e3
Ulad Kasach <uladkasach@gmail.com>
2021-09-08 08:55:13 -0400

    fix(decs): fine tune declarations to match lambda-services currently existing

--- src/practices/serverless/best-practice/serverless.yml.declapract.ts
+++ src/practices/serverless/best-practice/serverless.yml.declapract.ts
@@ -1,3 +1,4 @@
 import { FileCheckType } from 'declapract';
+import { FileCheckDeclaration } from 'declapract/dist/domain';
 
 export const check = FileCheckType.CONTAINS; // i.e., check that the contents of the file contains what's declared (default is equals)
