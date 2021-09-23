c2c6c203b129ba504ca3f98803cb27832fbf10a2
Ulad Kasach <uladkasach@gmail.com>
2021-09-23 08:30:36 -0400

    fix(testing): detect and fix another acceptance test bad practice

---
+++ src/practices/testing/bad-practices/old-acceptance-test-utils-2/acceptance/lambdas/*.ts.declapract.test.ts
@@ -0,0 +1,39 @@
+import { fix } from './*.ts.declapract';
+
+describe('*.ts.declapract', () => {
+  it('should be able to fix a test file using old acceptance test pattern', async () => {
+    const exampleOldPatternContents = `
+import { invokeLambdaForTesting } from 'simple-lambda-testing-methods';
+
+import { stage } from '../../src/utils/environment';
+import { locally } from '../__test_utils__/environment';
+
+describe('addDecoratedImage', () => {
+  it('it should be possible to add a decorated image', async () => {
+    // create the curation;
+    const response = await invokeLambdaForTesting({
+      service: 'svc-images',
+      function: 'addDecoratedImage',
+      locally,
+      stage,
+      event: {
+        url:
+          'https://some.url',
+        description: 'Bo Vine at his gig, moooooin the lawn',
+        focalArea: null,
+      },
+    });
+    expect(response.image).toHaveProperty('uuid');
+    expect(response.image.uuid).toContain('-'); // i.e., its a proper uuid
+  });
+});
+    `.trim();
+    const { contents: fixedContents } = await fix(exampleOldPatternContents, {
+      projectVariables: { serviceName: 'svc-notifications' },
+    } as any);
+    expect(fixedContents).toContain(
+      `import { locally } from '../environment';`,
+    );
+    expect(fixedContents).toMatchSnapshot();
+  });
+});
---
+++ src/practices/testing/bad-practices/old-acceptance-test-utils-2/acceptance/lambdas/*.ts.declapract.ts
@@ -0,0 +1,22 @@
+import { FileCheckFunction, FileFixFunction } from 'declapract';
+
+export const check: FileCheckFunction = (contents) => {
+  if (
+    contents?.includes(
+      `import { locally } from '../__test_utils__/environment';`,
+    )
+  )
+    return; // then it matches bad practice
+  throw new Error('does not match bad practice');
+};
+
+export const fix: FileFixFunction = async (contents, context) => {
+  if (!contents) return {}; // do nothing if no contents. really, this shouldn't have been called
+  return {
+    contents: contents.replace(
+      // replace the bad import
+      `import { locally } from '../__test_utils__/environment';`,
+      `import { locally } from '../environment';`,
+    ),
+  };
+};
