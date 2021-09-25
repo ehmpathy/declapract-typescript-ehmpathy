f09e818b4e4128607ad125bd7e14a0061ff8e6f8
Ulad Kasach <uladkasach@gmail.com>
2021-09-25 08:49:16 -0400

    fix(config): add checks and fixes for using old config path

---
+++ src/practices/config/bad-practices/in-util-dir-directly/src/**/*.ts.declapract.ts
@@ -0,0 +1,16 @@
+import { FileCheckFunction, FileFixFunction } from 'declapract';
+
+export const check: FileCheckFunction = (contents) => {
+  if (contents?.includes(`../utils/config'`)) return; // if it includes this string, then its a bad practice
+  throw new Error('its not bad practice'); // otherwise, not bad practice
+};
+
+export const fix: FileFixFunction = (contents) => {
+  if (!contents) return {}; // should not be called if no contents
+  return {
+    contents: contents.replace(
+      /\.\.\/utils\/config'/g,
+      "../utils/config/getConfig'",
+    ),
+  };
+};
