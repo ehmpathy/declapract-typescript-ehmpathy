ff9b0fbb162c0daba28d9d87360290aeb3ad66c4
Ulad Kasach <uladkasach@gmail.com>
2021-09-26 17:11:17 -0400

    fix(testing): add fix to another variant of old acceptance test utils

---
+++ src/practices/testing/bad-practices/old-acceptance-test-utils-3/acceptance/lambdas/*.ts.declapract.ts
@@ -0,0 +1,18 @@
+import { FileCheckFunction, FileFixFunction } from 'declapract';
+
+export const check: FileCheckFunction = (contents) => {
+  if (contents?.includes(`import { locally } from '../_utils/environment';`))
+    return; // then it matches bad practice
+  throw new Error('does not match bad practice');
+};
+
+export const fix: FileFixFunction = async (contents, context) => {
+  if (!contents) return {}; // do nothing if no contents. really, this shouldn't have been called
+  return {
+    contents: contents.replace(
+      // replace the bad import
+      `import { locally } from '../_utils/environment';`,
+      `import { locally } from '../environment';`,
+    ),
+  };
+};
