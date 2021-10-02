223bdcb539e19594877bf4d729ad3a88654050ff
Ulad Kasach <uladkasach@gmail.com>
2021-10-02 14:23:12 -0400

    fix(config): fix parameter store namespace defiend in tf

--- src/practices/config/best-practice/provision/aws/product/parameter-store.tf
+++ src/practices/config/best-practice/provision/aws/product/parameter-store.tf
@@ -1,3 +1,3 @@
 locals {
-  parameter_store_namespace = "@declapract{variable.organizationName}.${var.service}.${var.environment}" # keep in sync with `/config/${env}.json#parameterStoreNamespace
+  parameter_store_namespace = "@declapract{variable.organizationName}.${local.service}.${var.environment}" # keep in sync with `/config/${env}.json#parameterStoreNamespace
 }
