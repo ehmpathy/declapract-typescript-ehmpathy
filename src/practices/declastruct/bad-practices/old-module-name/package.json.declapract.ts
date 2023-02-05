15ed76ea9edcf290f831f2a141b314d767ece532
Ulad Kasach <uladkasach@gmail.com>
2023-02-05 14:52:15 -0500

    fix(deps): ensure uuid is v9 and old module name is a bad practice

---
+++ src/practices/declastruct/bad-practices/old-module-name/package.json.declapract.ts
@@ -0,0 +1,12 @@
+import { FileCheckFunction } from 'declapract';
+import expect from 'expect';
+
+export const check: FileCheckFunction = (contents) => {
+  expect(JSON.stringify(contents ?? 'null')).toMatchObject(
+    expect.objectContaining({
+      dependencies: expect.objectContaining({
+        'best-practices-typescript': expect.any(String),
+      }),
+    }),
+  );
+};
