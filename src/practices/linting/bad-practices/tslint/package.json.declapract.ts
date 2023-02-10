2a9eeb964854670e8fea5706560625ef60167f04
Ulad Kasach <uladkasach@gmail.com>
2023-02-10 10:55:44 -0500

    fix(linting): ensure to remove tslint package deps in badpractice too

---
+++ src/practices/linting/bad-practices/tslint/package.json.declapract.ts
@@ -0,0 +1,22 @@
+import { FileCheckFunction, FileFixFunction } from 'declapract';
+
+export const check: FileCheckFunction = (contents) => {
+  const packageJSONObject = JSON.parse(contents!);
+  if (Object.keys(packageJSONObject.dependencies).includes('uuidv4')) return; // matches
+  throw new Error('does not match bad practice');
+};
+
+export const fix: FileFixFunction = (contents) => {
+  if (!contents) return {}; // should not occur
+  const packageJSONObject = JSON.parse(contents);
+  const fixedPackageJSONObject = {
+    ...packageJSONObject,
+    dependencies: {
+      ...packageJSONObject.dependencies,
+      tslint: undefined,
+      'tslint-config-airbnb': undefined,
+      'tslint-consistent-codestyle': undefined,
+    },
+  };
+  return { contents: JSON.stringify(fixedPackageJSONObject, null, 2) };
+};
