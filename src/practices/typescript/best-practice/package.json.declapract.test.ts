74882eb098ae6497b41e34339f8d7e25f8238f5d
Ulad Kasach <uladkasach@gmail.com>
2023-07-15 20:48:10 -0700

    fix(usecase): ensure build:artifact only attempted if project uses artifact practice

---
+++ src/practices/typescript/best-practice/package.json.declapract.test.ts
@@ -0,0 +1,18 @@
+import { contents } from './package.json.declapract';
+
+describe('package.json', () => {
+  it('should include build:artifact when project uses artifact practice', async () => {
+    const declaredBestPracticeContents = await contents({
+      projectPractices: ['artifact'],
+    } as any);
+    expect(declaredBestPracticeContents).toContain('build:artifact');
+    expect(declaredBestPracticeContents).toMatchSnapshot();
+  });
+  it('should include not include build:artifact when project does not use artifact practice', async () => {
+    const declaredBestPracticeContents = await contents({
+      projectPractices: [],
+    } as any);
+    expect(declaredBestPracticeContents).not.toContain('build:artifact');
+    expect(declaredBestPracticeContents).toMatchSnapshot();
+  });
+});
--- src/practices/typescript/best-practice/package.json.declapract.ts
+++ src/practices/typescript/best-practice/package.json.declapract.ts
@@ -1,3 +1,18 @@
-import { FileCheckType } from 'declapract';
+import { FileCheckType, FileContentsFunction } from 'declapract';
+
+import { readFile } from '../../../utils/readFile';
+
+export const contents: FileContentsFunction = async (context) => {
+  // grab the superset of best practices content
+  const contentsSuperset = await readFile(`${__dirname}/package.json`);
+
+  // remove things that are irrelevant, based on other practices in use in project
+  let contents = contentsSuperset;
+  if (!context.projectPractices.includes('artifact'))
+    contents = contents.replace(` && npm run build:artifact`, '');
+
+  // return the narrowed contents
+  return contents;
+};
 
 export const check = FileCheckType.CONTAINS;
