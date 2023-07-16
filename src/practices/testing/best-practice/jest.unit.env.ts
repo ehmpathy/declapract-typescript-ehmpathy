6ceb9e18f1989d41c424cd7cff5a4facccdd20c0
Ulad Kasach <uladkasach@gmail.com>
2023-07-15 21:30:54 -0700

    fix(test): remove dependency of env from testing

--- src/practices/testing/best-practice/jest.unit.env.ts
+++ src/practices/testing/best-practice/jest.unit.env.ts
@@ -1,12 +1,16 @@
-import { stage, Stage } from './src/utils/environment';
-
 jest.mock('./src/utils/config/getConfig', () => ({
   getConfig: jest.fn().mockImplementation(() => require('./config/test.json')), // mock that getConfig just returns plaintext test env config in unit tests
 }));
 
 /**
- * sanity check that unit tests are only run in 'test' environment
- * - if they are run in prod environment, we could load a bunch of junk data into our prod databases, which would be no bueno
+ * sanity check that unit tests are only run the 'test' environment
+ *
+ * usecases
+ * - prevent polluting prod state with test data
+ * - prevent executing financially impacting mutations
  */
-if (stage !== Stage.TEST && process.env.I_KNOW_WHAT_IM_DOING !== 'true')
+if (
+  process.env.NODE_ENV !== 'test' &&
+  process.env.I_KNOW_WHAT_IM_DOING !== 'true'
+)
   throw new Error(`unit-test is not targeting stage 'test'`);
