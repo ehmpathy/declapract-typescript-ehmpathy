5357d7185fdd2c47631830750b40c18e8dbb5d69
Ulad Kasach <uladkasach@gmail.com>
2023-07-15 21:21:23 -0700

    fix(cicd): call integration-test-db --if-present

--- src/practices/cicd-package/best-practice/package.json.declapract.ts
+++
@@ -1,4 +0,0 @@
-import { FileCheckType } from 'declapract';
-
-// TODO: move this check out into the function that defines `fix:format`, and have it check whether it should include terraform instead
-export const check = FileCheckType.CONTAINS;
