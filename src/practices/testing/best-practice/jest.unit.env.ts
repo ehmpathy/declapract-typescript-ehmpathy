4053c58ae0c555a0ab91ba7f8acbf0824a86dafa
Ulad Kasach <uladkasach@gmail.com>
2023-07-28 07:59:53 -0700

    fix(testing): ensure tests against nontest stage are guarded against better

--- src/practices/testing/best-practice/jest.unit.env.ts
+++ src/practices/testing/best-practice/jest.unit.env.ts
@@ -10,7 +10,7 @@ jest.mock('./src/utils/config/getConfig', () => ({
  * - prevent executing financially impacting mutations
  */
 if (
-  process.env.NODE_ENV !== 'test' &&
+  (process.env.NODE_ENV !== 'test' || process.env.STAGE) &&
   process.env.I_KNOW_WHAT_IM_DOING !== 'true'
 )
   throw new Error(`unit-test is not targeting stage 'test'`);
