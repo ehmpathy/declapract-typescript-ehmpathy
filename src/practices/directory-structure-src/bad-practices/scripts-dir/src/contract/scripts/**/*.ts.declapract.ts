d61c4b051210d71b2d89c8465c2ab15810a93508
Ulad Kasach <uladkasach@gmail.com>
2023-07-30 18:29:27 -0700

    fix(src): add scripts directory as bad practice; prefer commands dir

---
+++ src/practices/directory-structure-src/bad-practices/scripts-dir/src/contract/scripts/**/*.ts.declapract.ts
@@ -0,0 +1,12 @@
+import { FileCheckType, FileFixFunction } from 'declapract';
+
+export const check = FileCheckType.EXISTS; // if we see that files with this path exist, its bad practice
+
+export const fix: FileFixFunction = (_, context) => {
+  return {
+    relativeFilePath: context.relativeFilePath.replace(
+      'src/contract/scripts/',
+      'src/contract/commands/',
+    ),
+  };
+};
