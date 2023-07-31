b97b47ee825dc0ae1f10b2041275d33fe5c45379
Ulad Kasach <uladkasach@gmail.com>
2023-07-30 18:49:40 -0700

    fix(practs): bump best practs for faster cicd

--- src/utils/readFile.ts
+++ src/utils/readFile.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
 import util from 'util';
 
-export const readFile = async (filePath: string) =>
+export const readFile = async (filePath: string): Promise<string> =>
   util.promisify(fs.readFile)(filePath, 'utf-8');
