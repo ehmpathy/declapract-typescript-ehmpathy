2f92c640a0f3f1d911057a2b622238331ba43b1d
Ulad Kasach <uladkasach@gmail.com>
2024-04-20 17:49:09 -0400

    feat(context): add withDatabaseContext method to begin support of contexts

---
+++ src/practices/persist-with-rds/best-practice/src/utils/database/withDatabaseContext.ts
@@ -0,0 +1,45 @@
+import {
+  DatabaseConnection,
+  getDatabaseConnection,
+} from './getDatabaseConnection';
+
+/**
+ * wraps the function to provide a managed database connection to it's context, if one was not already passed in
+ *
+ * note
+ * - manages the database connection it gives to the function by opening it and closing it once the function finishes
+ * - if a database connection was passed in as part of the arguments, it will instead use that one and will not close it once it finishes (useful for transactions)
+ *
+ * example usage:
+ * ```
+ *   const findById = withDatabaseConnection(({ id }: { id: string }, context: { dbConnection: DatabaseConnection }) => {
+ *     // do logic with dbConnection
+ *   });
+ *   // ...
+ *   await findById({ id: 821 }); // note how we don't have to pass in the dbConnection
+ * ```
+ */
+export const withDatabaseContext = <
+  P1,
+  P2 extends { dbConnection: DatabaseConnection },
+  R,
+>(
+  logic: (input: P1, context: P2) => R | Promise<R>,
+) => {
+  return async (
+    input: P1,
+    context: Omit<P2, 'dbConnection'> & { dbConnection?: DatabaseConnection },
+  ) => {
+    // open the db connection, if one was not given
+    const dbConnection =
+      context.dbConnection ?? (await getDatabaseConnection());
+
+    // try and run the logic with db connection
+    try {
+      return await logic(input, { ...context, dbConnection } as P2); // as P2 because: https://github.com/microsoft/TypeScript/issues/35858
+    } finally {
+      // make sure to close the db connection, both when `logic` throws an error or succeeds
+      if (!context.dbConnection) await dbConnection.end();
+    }
+  };
+};
