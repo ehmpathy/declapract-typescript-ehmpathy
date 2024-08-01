db74e3cc4fbd9e955a244d331193561326d40eb6
Ulad Kasach <uladkasach@gmail.com>
2024-08-01 07:33:57 -0400

    feat(config): declare org, project, env, and aws on config

--- src/practices/config/best-practice/src/utils/config/Config.ts
+++ src/practices/config/best-practice/src/utils/config/Config.ts
@@ -1,1 +1,10 @@
-export interface Config {}
+export interface Config {
+  organization: string;
+  project: string;
+  environment: {
+    access: string;
+  };
+  aws: {
+    account: string;
+    namespace: string;
+  };
--- src/practices/config/best-practice/src/utils/config/Config.ts.declapract.ts
+++ src/practices/config/best-practice/src/utils/config/Config.ts.declapract.ts
@@ -1,7 +1,16 @@
+import { UnexpectedCodePathError } from '@ehmpathy/error-fns';
 import { FileCheckType, FileFixFunction } from 'declapract';
 
-export const check = FileCheckType.EXISTS;
+export const check = FileCheckType.CONTAINS;
 
-export const fix: FileFixFunction = () => ({
-  contents: `export interface Config {}`,
-});
+export const fix: FileFixFunction = (contents, { declaredFileContents }) =>
+  !declaredFileContents
+    ? UnexpectedCodePathError.throw('expected declared file contents to exist')
+    : !contents
+    ? { contents: [declaredFileContents.trim(), '}'].join('\n') }
+    : {
+        contents: contents.replace(
+          'export interface Config {',
+          declaredFileContents.trim(), // add the prefix
+        ),
+      };
