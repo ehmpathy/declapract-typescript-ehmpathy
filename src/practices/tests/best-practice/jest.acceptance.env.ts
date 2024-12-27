c4869d4cb0e410598fdafec73325b232eca2dc73
Ulad Kasach <uladkasach@gmail.com>
2024-12-27 14:21:52 -0800

    fix(tests): fail fast on missed creds for acceptance tests

--- src/practices/tests/best-practice/jest.acceptance.env.ts
+++ src/practices/tests/best-practice/jest.acceptance.env.ts
@@ -1,2 +1,14 @@
 // eslint-disable-next-line no-undef
 jest.setTimeout(90000); // we're calling downstream apis
+
+/**
+ * .what = verify that the env has sufficient auth to run the tests; otherwise, fail fast
+ * .why =
+ *   - prevent time wasted waiting on tests to fail due to lack of credentials
+ *   - prevent time wasted debugging tests which are failing due to hard-to-read missed credential errors
+ */
+const isAuthed = process.env.AWS_PROFILE || process.env.AWS_ACCESS_KEY_ID;
+if (!isAuthed)
+  throw new Error(
+    'no aws credentials present. please authenticate with aws to run acceptance tests',
+  );
