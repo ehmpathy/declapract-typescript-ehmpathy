b888de253f63c000698a5a37c6ac28c4727c5893
Ulad Kasach <uladkasach@gmail.com>
2025-02-23 16:38:17 -0800

    fix(jest): ensure isauthed declaration doesnt cause init errors

--- src/practices/tests/best-practice/jest.acceptance.env.ts
+++ src/practices/tests/best-practice/jest.acceptance.env.ts
@@ -7,8 +7,7 @@ jest.setTimeout(90000); // we're calling downstream apis
  *   - prevent time wasted waiting on tests to fail due to lack of credentials
  *   - prevent time wasted debugging tests which are failing due to hard-to-read missed credential errors
  */
-const isAuthed = process.env.AWS_PROFILE || process.env.AWS_ACCESS_KEY_ID;
-if (!isAuthed)
+if (!(process.env.AWS_PROFILE || process.env.AWS_ACCESS_KEY_ID))
   throw new Error(
     'no aws credentials present. please authenticate with aws to run acceptance tests',
   );
--- src/practices/tests/best-practice/jest.integration.env.ts
+++ src/practices/tests/best-practice/jest.integration.env.ts
@@ -20,8 +20,7 @@ if (
  *   - prevent time wasted waiting on tests to fail due to lack of credentials
  *   - prevent time wasted debugging tests which are failing due to hard-to-read missed credential errors
  */
-const isAuthed = process.env.AWS_PROFILE || process.env.AWS_ACCESS_KEY_ID;
-if (!isAuthed)
+if (!(process.env.AWS_PROFILE || process.env.AWS_ACCESS_KEY_ID))
   throw new Error(
     'no aws credentials present. please authenticate with aws to run integration tests',
   );
