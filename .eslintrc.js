d69bc6d1002892534a79ac053261924941194bba
Ulad Kasach <uladkasach@gmail.com>
2025-11-23 17:40:54 -0600

    fix(cicd): ensure depcheckr knows unused-imports eslint plugin is used

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     'airbnb-typescript/base', // uses the airbnb recommended rules
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
+  plugins: ['unused-imports'],
   parserOptions: {
     project: './tsconfig.json',
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
@@ -33,6 +34,7 @@ module.exports = {
     ],
     '@typescript-eslint/no-explicit-any': 'off', // sometimes this is a valid definition
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'unused-imports/no-unused-imports': 'error', // auto-fixable rule to remove unused imports
     'import/no-cycle': 'off',
     'max-classes-per-file': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
