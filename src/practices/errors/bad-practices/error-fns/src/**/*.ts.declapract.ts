e2bbf0dd0846ec69b2d72269ff42efbfef5165ed
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2025-12-02 05:20:04 -0600

    fix(deps): autofix error-fns => helpful-errors cutover (#215)

---
+++ src/practices/errors/bad-practices/error-fns/src/**/*.ts.declapract.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { type FileCheckFunction, type FileFixFunction } from 'declapract';
+
+export const check: FileCheckFunction = (contents) => {
+  if (contents?.includes("from '@ehmpathy/error-fns'")) return; // matches if it includes this
+  throw new Error('does not match bad practice'); // does not otherwise
+};
+
+export const fix: FileFixFunction = (contents) => {
+  if (!contents) return {}; // do nothing if no contents
+  return {
+    contents: contents.replace(
+      /from '@ehmpathy\/error-fns'/g,
+      "from 'helpful-errors'",
+    ),
+  };
+};
