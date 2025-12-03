ccd64974a34fbe3007b4a181a0f6309ce5d07086
Ulad Kasach <uladkasach@gmail.com>
2025-12-03 06:30:28 -0600

    fix(lint): bump commitlint version

---
+++ src/practices/conventional-commits/bad-practices/commitlint/package.json.declapract.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FileCheckType, type FileFixFunction } from 'declapract';
+
+export const check = FileCheckType.CONTAINS;
+
+export const fix: FileFixFunction = (contents) => {
+  if (!contents) return { contents }; // do nothing if no contents
+  const packageJSON = JSON.parse(contents);
+  const updatedPackageJSON = {
+    ...packageJSON,
+    devDependencies: {
+      ...packageJSON.devDependencies,
+      commitlint: undefined,
+    },
+  };
+  return {
+    contents: JSON.stringify(updatedPackageJSON, null, 2),
+  };
+};
