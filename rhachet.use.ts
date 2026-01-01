6532697668efb0765b94bc5f03c75763c72203d7
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2026-01-01 12:20:30 -0600

    fix(deps): leverage rhachet roles bhrain and bhuild (#337)

--- rhachet.use.ts
+++ rhachet.use.ts
@@ -1,6 +1,8 @@
 import type { InvokeHooks, RoleRegistry } from 'rhachet';
 
+import { getRoleRegistry as getRoleRegistryBhrain, getInvokeHooks as getInvokeHooksBhrain } from 'rhachet-roles-bhrain';
+import { getRoleRegistry as getRoleRegistryBhuild, getInvokeHooks as getInvokeHooksBhuild } from 'rhachet-roles-bhuild';
 import { getRoleRegistry as getRoleRegistryEhmpathy, getInvokeHooks as getInvokeHooksEhmpathy } from 'rhachet-roles-ehmpathy';
 
-export const getRoleRegistries = (): RoleRegistry[] => [getRoleRegistryEhmpathy()];
-export const getInvokeHooks = (): InvokeHooks[] => [getInvokeHooksEhmpathy()];
+export const getRoleRegistries = (): RoleRegistry[] => [getRoleRegistryBhrain(), getRoleRegistryBhuild(), getRoleRegistryEhmpathy()];
+export const getInvokeHooks = (): InvokeHooks[] => [getInvokeHooksBhrain(), getInvokeHooksBhuild(), getInvokeHooksEhmpathy()];
